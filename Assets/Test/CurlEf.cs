f35af71d8ee3bd856a9191bdd7044b6d5d618130
Daniel Lupascu <liviudaniel.lupascu@gmail.com>
2019-03-14 22:56:42 +0200

    Simple drag logic for LD, RD 4h

--- Assets/Test/CurlEf.cs
+++ Assets/Test/CurlEf.cs
@@ -5,20 +5,64 @@ using System.Collections;
 public class CurlEf : MonoBehaviour {
 
     public Transform _Front;
+
     public Transform _Mask;
     public Transform _GradOutter;
 
+    public Transform handle;
+    
     public Transform _PosLD;
     public Transform _PosRD;
 
     Vector3 _Pos;
     
+    [SerializeField]
+    RectTransform _rectTransform;
+    [SerializeField]
+    RectTransform _FrontRectTransform;
+
+    int corner = 4;
+    public int StartCorner = 4;
+
+    public Vector3 dragOffset = Vector3.zero;
+
+    void Start()
+    {
+        corner = StartCorner;
+        dragOffset = Vector3.zero;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    bool changedCorner = false;
+
     void LateUpdate()
     {
+//        _Front.position = Vector3.Lerp(_Front.position, handle.position + dragOffset, Time.fixedDeltaTime*10);
+        _Front.position = handle.position+ dragOffset;        
+
         transform.position = _Pos;
         transform.eulerAngles = Vector3.zero;
 
-        pivotLD();
+        switch (corner)
+        {
+            case 3:
+                pivotRD();
+                break;
+            case 4:
+                pivotLD();
+                break;
+        }
+
+        if (changedCorner)
+        {
+            changedCorner = false;
+            LateUpdate();
+            return;
+        }
 
         transform.position = _Pos;
         transform.eulerAngles = Vector3.zero;
@@ -28,14 +72,27 @@ public class CurlEf : MonoBehaviour {
     void pivotLD()
     {
         //shift pivot
-        
+        _rectTransform.pivot = new Vector2(0, 0);
+        _FrontRectTransform.pivot = new Vector2(0,1);
         _Pos = _PosLD.position;
+//        _FrontRectTransform.anchorMin = new Vector2(1,0);
+//        _FrontRectTransform.anchorMax = new Vector2(1,0);
         
         
         Vector3 pos = _Front.localPosition;
         float theta = Mathf.Atan2(pos.y, pos.x) * 180.0f / Mathf.PI;
 
-        if (theta <= 0.0f || theta >= 90.0f) return;
+        if ( theta >= 90.0f) // to much left
+        {
+            corner = 3;
+            dragOffset += _PosRD.position - _PosLD.position;
+            changedCorner = true;
+            return;
+        }
+        if (theta <= 0.0f) //too much down
+        {
+            return;
+        }
 
         float deg = -(90.0f - theta) * 2.0f;
         _Front.eulerAngles = new Vector3(0.0f, 0.0f, deg);
@@ -45,6 +102,42 @@ public class CurlEf : MonoBehaviour {
 
         _GradOutter.position = _Mask.position;
         _GradOutter.eulerAngles = new Vector3(0.0f, 0.0f, deg * 0.5f + 90.0f);
+    }
+    
+    void pivotRD()
+    {
+        //shift pivot
+        _rectTransform.pivot = new Vector2(1, 0);
+        _FrontRectTransform.pivot = new Vector2(1,1);
+        _Pos = _PosRD.position;
+//        _FrontRectTransform.anchorMin = new Vector2(1,0);
+//        _FrontRectTransform.anchorMax = new Vector2(1,0);
+        
+        
+        Vector3 pos = _Front.localPosition;
+        float theta = Mathf.Atan2(pos.y, -pos.x) * 180.0f / Mathf.PI;
+
+        if (theta >= 90.0f)
+        {
+            corner = 4;
+            dragOffset -=  _PosRD.position - _PosLD.position;
+            changedCorner = true;
+            return;
+        }
+
+        if (theta <= 0.0f)
+        {
+            return;
+        }
+
+        float deg = -(90.0f - theta) * 2.0f;
+        _Front.eulerAngles = new Vector3(0.0f, 0.0f, -deg);
+
+        _Mask.position = (transform.position + _Front.position) * 0.5f;
+        _Mask.eulerAngles = new Vector3(0.0f, 0.0f, -deg*0.5f);
+
+        _GradOutter.position = _Mask.position;
+        _GradOutter.eulerAngles = new Vector3(0.0f, 0.0f, -deg * 0.5f + 90.0f);
     }
     
     
